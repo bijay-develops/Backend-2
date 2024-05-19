3c207a20e527bd642ad495e2bb11250e8027069b
Bijay B.K <bijaybk307@gmail.com>
2024-05-20 00:16:39 +0545

    day 1 commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    res.render("index");
+}) 
+
+app.get("/profile/bijay", function(req, res){
+    res.send("chaliraaxa");
+}) 
+
+app.listen(3000, function(){
+    console.log("Its Running");
+})
